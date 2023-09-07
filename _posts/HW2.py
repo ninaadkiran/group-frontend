89952a58916b965171c51094451f9f40730afedf
ninaadkiran <ninaadkiran@gmail.com>
2023-09-07 21:51:22 +0000

    :)

--- _posts/HW2.py
+++ _posts/HW2.py
@@ -29,4 +29,3 @@ if answer.lower()=='yes':
 print('Thankyou for Playing this small quiz game, you attempted',score,"questions correctly!")
 mark=(score/total_questions)*100
 print('Marks obtained:',mark)
-print('BYE!')
--- compsci.md
+++ compsci.md
@@ -1,6 +1,6 @@
 ---
 layout: schedule
-title: Computer Science Lab Notebook
+title: Computer Science Time Box
 units: "1,2"
 course: compsci
 ---

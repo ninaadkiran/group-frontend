6619236e10923b707e973d96964d760e4e111034
JoshThinh <joshua.thinh@gmail.com>
2024-02-11 23:36:04 -0800

    added home page

---
+++ script/javascript/button.js
@@ -0,0 +1,11 @@
+function galaxygame() {
+    window.location.href = 'http://127.0.0.1:5500/_layouts/galaxy.html'; 
+}
+
+function dodgegame() {
+    window.location.href = 'http://127.0.0.1:5500/_layouts/galaxy.html'; 
+}
+
+function escapegame() {
+    window.location.href = 'http://127.0.0.1:5500/_layouts/galaxy.html'; 
+}
